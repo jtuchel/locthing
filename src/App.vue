24a72c8ecf4a804ea1fb8fa366fdc6969cea7c79
matthiashermsen <matthiashermsen@gmail.com>
2022-12-06 14:56:26 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useLocale } from 'vuetify'
+import { useI18n } from 'vue-i18n'
+
+const { current } = useLocale()
+const { t } = useI18n()
+
+setTimeout(() => {
+  current.value = 'de';
+}, 3000);
+</script>
+
+<template>
+  <div>
+    <div>content:</div>
+    <div>{{ t('title') }}</div>
+  </div>
+</template>
+
+<i18n>
+  {
+    "en": {
+      "title": "title | en"
+    },
+    "de": {
+      "title": "Titel | de"
+    }
+  }
+</i18n>
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { resolve, dirname } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/**'),
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+})
